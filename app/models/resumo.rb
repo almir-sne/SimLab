51ce8b1eb483fe0abb3765c927ca893b5f677525
Eliada <caio.c.salgado@gmail.com>
2013-03-14 15:26:48 -0300

    refatoração do horas no mes

--- app/models/resumo.rb
+++ app/models/resumo.rb
@@ -1,26 +1,24 @@
-class Resumo < ActiveRecord::Base
+class Resumo
 
-    def self.horas_no_mes(user)
-      meses = (1..12).to_a
-      hora_mes = Hash.new
+# devolve hash com chave os meses, conteudo um vetor de horas_feitas e horas_pretendidas.
+# exemplo: {1 => [100, 100], 2 => [100,100], 3 => [50,100]}
+  def self.horas_no_mes(user)
+    meses = (1..12).to_a
+    hora_mes = Hash.new
 
-      meses.each do |mes|
-        mes_atual = Date.new(Date.today.year, mes, 1)
-        proximo_mes = mes_atual.next_month - 1.day
+    meses.each do |mes|
+      mes_atual = Date.new(Date.today.year, mes, 1)
+      proximo_mes = mes_atual.next_month - 1.day
 
-        horas_pretendidas = Usuario.find(user.id).horario_mensal
-        horas_feitas = BancoDeHora.find_all_by_usuario_id_and_data(user.id, mes_atual..proximo_mes).map{ |bh| bh.horas }
-        horas_feitas = horas_feitas.inject{|sum,x| sum + x }
+      horas_pretendidas = user.horario_mensal
+      horas_feitas = user.banco_de_horas.where{data >> (mes_atual..proximo_mes)}.all.map{ |bh| bh.horas }
+      #horas_feitas = BancoDeHora.find_all_by_usuario_id_and_data(user.id, mes_atual..proximo_mes).map{ |bh| bh.horas }
+      horas_feitas = horas_feitas.inject{|sum,x| sum + x }
 
-        hora_mes[mes] = [horas_feitas, horas_pretendidas]
-      end
-      hora_mes
+      hora_mes[mes] = [horas_feitas, horas_pretendidas]
     end
-
-
-
-    private
-
+    hora_mes
+  end
 
 
 end
