52d5f256608c27c6d9de8a9d4719d0fb13527c68
Eliada <caio.c.salgado@gmail.com>
2013-03-14 15:25:36 -0300

    bug fix

--- app/controllers/resumo_controller.rb
+++ app/controllers/resumo_controller.rb
@@ -2,7 +2,7 @@ class ResumoController < ApplicationController
   before_filter :authenticate_usuario!, :except => [:show, :index]
 
   def index
-    @horas = Resumo.horas_do_mes current_usuario
+    @horas = Resumo.horas_no_mes current_usuario
   end
 
 end
--- app/models/banco_de_hora.rb
+++ app/models/banco_de_hora.rb
@@ -1,8 +1,4 @@
 class BancoDeHora < ActiveRecord::Base
   attr_accessible :data, :horas, :observacao, :projeto_id, :usuario_id
 
-  def horas
-    horas ||= 0
-  end
-
 end
