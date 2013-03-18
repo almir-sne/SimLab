21e208ad86e4e3c7722a4e111c3325536702e8cf
Caio Salgado <caio@montanha.(none)>
2013-03-18 20:23:20 -0300

    modifed view and create new one, but still work to do on it

--- app/controllers/resumo_controller.rb
+++ app/controllers/resumo_controller.rb
@@ -4,5 +4,13 @@ class ResumoController < ApplicationController
   def index
     @horas = Resumo.horas_no_mes current_usuario
   end
+  
+  def show
+  	tipo_de_consulta = params[:id]
+  	@horas_feitas
+  	if tipo_de_consulta == "hora_dia_p_pessoa"
+  		render :template =>"resumo/asd"
+  	end
+  end
 
 end
