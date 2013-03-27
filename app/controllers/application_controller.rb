841bdb7c48c1c71d92586bbb0ac620848d5045f9
labarq <labarq@labarq.(none)>
2013-03-27 20:25:34 -0300

    Terminado a tela de login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+    def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
 end
