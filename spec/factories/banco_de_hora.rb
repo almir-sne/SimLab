e2efc3b0e141c52b90a8dfba52831f2e8586462c
Eliada <caio.c.salgado@gmail.com>
2013-06-24 15:32:19 -0300

    blank tests erased

--- spec/factories/banco_de_hora.rb
+++
@@ -1,5 +0,0 @@
-FactoryGirl.define do
-  factory :banco_de_hora do
-    projeto
-  end
-end
--- spec/model/banco_de_horas_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-
-describe BancoDeHora do
-  it { should validate_presence_of :projeto_id }
-  it { should validate_presence_of :usuario_id }
-end
