3de749bffde1fd0b9501fdacd5e456b1cde2a6c7
Eliada <caio.c.salgado@gmail.com>
2013-08-28 18:42:04 -0300

    modificado desenvolvedor a coordenador, para usuario normal e diretor

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130828175458) do
+ActiveRecord::Schema.define(:version => 20130828213525) do
 
   create_table "addresses", :force => true do |t|
     t.string   "state"
