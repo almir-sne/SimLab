4be0ac6c34bcd146d753233214c8d2032cde5428
Almir <almir.sne@gmail.com>
2013-09-04 16:34:42 -0300

    Bug na migração 20130903172053 resolvido

--- db/migrate/20130903172053_add_data_to_atividade.rb
+++ db/migrate/20130903172053_add_data_to_atividade.rb
@@ -2,9 +2,13 @@ class AddDataToAtividade < ActiveRecord::Migration
   def up
     add_column :atividades, :data, :date
     Atividade.all.each do |a|
-      unless a.dia.blank?
-        a.data = a.dia.data
-        a.save
+      begin
+        unless a.dia.blank?
+          a.data = a.dia.data
+          a.save
+        end
+      rescue => ex
+        nil
       end
     end
   end
