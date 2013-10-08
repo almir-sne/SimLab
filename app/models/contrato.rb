b3e30b73ca7577e1a47e1757ec3e48d8a5b2ae28
Eliada <caio.c.salgado@gmail.com>
2013-10-08 17:35:30 -0300

    feita a adição de dia_de_inicio_de_periodo no contrato

--- app/models/contrato.rb
+++ app/models/contrato.rb
@@ -1,3 +1,14 @@
 class Contrato < ActiveRecord::Base
-  attr_accessible :contratante, :fim, :funcao, :hora_mes, :inicio, :tipo, :usuario_id, :valor_hora
+  attr_accessible :contratante, :fim, :funcao, :hora_mes, :inicio, :tipo, :usuario_id, :valor_hora, :dia_inicio_periodo
+
+  def periodo_vigente(data)
+   dia_corrente = data.day
+   intervalo_data = data + (dia_corrente - dia_inicio_periodo).day
+   if dia_corrente >= dia_inicio_periodo
+    (intervalo_data)..(intervalo_data + 1.month)
+   else
+    (intervalo_data - 1.month)..(intervalo_data)
+   end
+  end
+
 end
