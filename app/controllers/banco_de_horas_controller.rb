2827ee7e2d6c2c7de2aeed46d1c118413f981aa8
Vinicius G de Rezende <rezende@ime.usp.br>
2013-10-09 14:54:05 -0300

    Primeira versão do calendário

--- app/controllers/banco_de_horas_controller.rb
+++ app/controllers/banco_de_horas_controller.rb
@@ -33,15 +33,15 @@ class BancoDeHorasController < ApplicationController
     end
   end
 
-  #def show_mes2
-    #@year = params[:ano].nil? ? Date.today.year : params[:ano]
-    #@user = params[:user_id].nil? ? current_user : Usuario.find(params[:user_id])
-    #mes_num = params[:mes].nil? ? Date.today.month : Usuario.find(params[:mes])
-    #@mes_model = Mes.find_by_ano_and_usuario_id_and_numero @year, @user.id, mes_num
-    #@inicio_do_mes = Date.new(@mes_model.ano, mes_num, 1) 
-  #end
-  
   def show_mes
+    @year = params[:ano].nil? ? Date.today.year : params[:ano]
+    @user = params[:user_id].nil? ? current_user : Usuario.find(params[:user_id])
+    mes_num = params[:mes].nil? ? Date.today.month : Usuario.find(params[:mes])
+    @mes_model = Mes.find_by_ano_and_usuario_id_and_numero @year, @user.id, mes_num
+    @inicio_do_mes = Date.new(@mes_model.ano, mes_num, 1)
+  end
+  
+  def show_mes2
     @year = params[:year].nil? ? Date.today.year : params[:year]
     @user = params[:user_id].nil? ? current_user : Usuario.find(params[:user_id])
     query = Mes.find_all_by_ano_and_usuario_id @year, @user.id
