bb7ac38a8bf56c3eb246949846e87845f9d343f9
Eliada <caio.c.salgado@gmail.com>
2013-10-10 12:45:56 -0300

    refaz a função de periodos

--- app/models/contrato.rb
+++ app/models/contrato.rb
@@ -2,13 +2,16 @@ class Contrato < ActiveRecord::Base
   attr_accessible :contratante, :fim, :funcao, :hora_mes, :inicio, :tipo, :usuario_id, :valor_hora, :dia_inicio_periodo
 
   def periodo_vigente(data)
-   dia_corrente = data.day
-   intervalo_data = data + (dia_corrente - dia_inicio_periodo).day
-   if dia_corrente >= dia_inicio_periodo
-    (intervalo_data)..(intervalo_data + 1.month)
-   else
-    (intervalo_data - 1.month)..(intervalo_data)
-   end
+    if data.day < dia_inicio_periodo
+      inicio_periodo = (data.change(day: dia_inicio_periodo) - 1.month)
+      fim_periodo = data.change(day: dia_inicio_periodo) - 1.day
+    else
+      inicio_periodo = (data.change(day: dia_inicio_periodo))
+      fim_periodo = data.change(day: dia_inicio_periodo) - 1.day + 1.month
+    end
+    inicio_periodo = inicio if inicio_periodo < inicio
+    fim_periodo = fim if fim_periodo > fim
+    inicio_periodo .. fim_periodo
   end
 
 end
