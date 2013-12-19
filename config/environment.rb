bd47b812b3ab0535fb0d375b2a468d3786358c55
Daniel Barbosa Stein <ksatechnology@gmail.com>
2013-12-19 10:18:50 -0200

    Fix

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 SinLab::Application.initialize!
 
+#Global Functions
 def extract_tags(string)
     regex_tags = /(\[(.*?)\])+/
     preprocessed = string
