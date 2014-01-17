c54ba4d2d78d64313f651c7e1ca2bb838a5f3eb4
Vinicius G de Rezende <rezende@ime.usp.br>
2014-01-17 08:23:14 -0200

    Retirado arquivos inúteis

--- app/models/campo_dado.rb
+++
@@ -1,7 +0,0 @@
-class CampoDado < ActiveRecord::Base
-  belongs_to :campo_projeto
-  belongs_to :usuario
-  
-  validates_presence_of :campo_projeto_id
-  validates_presence_of :usuario_id
-end
--- app/models/campo_projeto.rb
+++
@@ -1,8 +0,0 @@
-class CampoProjeto < ActiveRecord::Base
-  belongs_to :projeto
-  
-  has_many :campo_dados
-   
-  validates_presence_of :projeto_id
-
-end
