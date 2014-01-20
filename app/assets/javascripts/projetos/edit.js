426828ea4f94229dca8a621030a9d211b1cde3c1
Almir <almir.sne@gmail.com>
2014-01-20 18:11:33 -0200

    Melhorias no layout da tela de edição de projetos, melhorias no seletor de membros da equipe

--- app/assets/javascripts/projetos/edit.js
+++ app/assets/javascripts/projetos/edit.js
@@ -1,61 +1,67 @@
-  window.NestedFormEvents.prototype.insertFields = function(content, assoc, link) {
+window.NestedFormEvents.prototype.insertFields = function(content, assoc, link) {
     if (assoc == "coordenacoes") {
-      var a = $(content).insertBefore($('#novo_item_coordenador'));
-      return a;
+        var a = $(content).insertBefore($('#novo_item_coordenador'));
+        return a;
     }
     else {
-      var a = $(content).insertBefore($('#novo_item'));
-      //@author rezende, stein
-      var tableData = a.children().eq(0);
-      var usuario_id_hidden_input = $(document.createElement('input'));
-      var new_random_id = a.children().eq(1).children().eq(0).attr('id').split('_')[3];
-      nome = ($('#equipe_autocomplete').val());
-      usuario_id_hidden_input.attr('id', 'projeto_workons_attributes_'+new_random_id+'_usuario_id');
-      usuario_id_hidden_input.attr('name', 'projeto[workons_attributes]['+new_random_id+'][usuario_id]');
-      usuario_id_hidden_input.attr('type', 'hidden');
-      $.ajax({
-        url: url,
-        data: {name: nome},
-        success: function(result) {
-          usuario_id_hidden_input.attr('value', result);
-        }
-      });
-      tableData.text(nome);
-      tableData.append(usuario_id_hidden_input);
-      return a;
-      }
-  }
+        var a = $(content).insertBefore($('#novo_item'));
+        //@author rezende, stein
+        var tableData = a.children().eq(0);
+        var usuario_id_hidden_input = $(document.createElement('input'));
+        var new_random_id = a.children().eq(1).children().eq(0).attr('id').split('_')[3];
+        id = $("#usuarios").val();
+        nome = $("#usuarios").find("option[value='" + id + "']").text();
+        usuario_id_hidden_input.attr({
+            id: 'projeto_workons_attributes_' + new_random_id + '_usuario_id',
+            name: 'projeto[workons_attributes][' + new_random_id + '][usuario_id]',
+            value: id,
+            type: 'hidden'
+        });
+        tableData.text(nome);
+        tableData.append(usuario_id_hidden_input);
+        return a;
+    }
+};
 
-  checkTrello(getBoards);
+checkTrello(getBoards);
 
-  $(document).ready(function() {
-    if($("input[name='super_projeto']:checked").val() == "true")
-      mostraFilhos();
+$(document).ready(function() {
+    if ($("input[name='super_projeto']:checked").val() == "true")
+        mostraFilhos();
     else
-      mostraPai();
-  })
+        mostraPai();
+})
 
-  $("#super_projeto_true").click(function(){
+$("#super_projeto_true").click(function() {
     mostraFilhos();
-  });
+});
 
-  $("#super_projeto_false").click(function(){
+$("#super_projeto_false").click(function() {
     mostraPai();
-  });
+});
 
-  function mostraPai(){
+function mostraPai() {
     $("#filhos").hide(200);
     $("#pai").show(200);
-  }
+}
 
-  function mostraFilhos(){
+function mostraFilhos() {
     $("#filhos").show(200);
     $("#pai").hide(200);
-  }
+}
 
-function set_autocomplete_equipe()
-{
-  $("#equipe_autocomplete").autocomplete({
+function set_autocomplete_equipe() {
+    $("#equipe_autocomplete").autocomplete({
         source: users_projeto
     });
 }
+
+$(".chosen-select").chosen({
+    width: "95%",
+    no_results_text: "Nenhum resultado encontrado",
+    placeholder_text_single: "Escolha um usuario"
+});
+
+$('.chosen-select').on('change', function(evt, params) {
+    $("#workon-add").click();
+});
