9c0f1b9592e1c5b5f9b532708d9faf7daef36fc2
Eliada <caio.c.salgado@gmail.com>
2014-02-12 16:26:42 -0200

    carrega os nomes dos cartões pais corretamente

--- app/assets/javascripts/cartoes/index.js
+++ app/assets/javascripts/cartoes/index.js
@@ -3,6 +3,7 @@ function selectPais(){
   var select = document.createElement("select");
   var cartoes_pais = $("#cartoes_pais_ids").val().split(" ");
   var cartoes_pais_trello = $("#cartoes_pais_trello_ids").val().split(" ");
+  var cartoes_pais_opcoes = new Array();
 
   select.setAttribute("name", "cartao_pai");
   select.setAttribute("id", "cartao_pai");
@@ -19,13 +20,14 @@ function selectPais(){
   select.appendChild(option);
 
   for(i=0; i<cartoes_pais.length; i++){
-    option = document.createElement("option");
-    option.setAttribute("value", cartoes_pais[i]);
     Trello.get("/cards/" + cartoes_pais_trello[i], function(card){
-      option.innerHTML = card.name
+      cartoes_pais_opcoes[i] = document.createElement("option");
+      cartoes_pais_opcoes[i].setAttribute("value", cartoes_pais[i]);
+      cartoes_pais_opcoes[i].innerHTML = card.name;
+      select.appendChild(cartoes_pais_opcoes[i]);
     })
-    select.appendChild(option);
   }
+
   document.getElementById("cartao_pai_select").appendChild(select);
 }
 loginTrello(selectPais)
